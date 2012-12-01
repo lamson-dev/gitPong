76e3eba2be3e3397f6150d5c9701225b16d51016
Son Nguyen <lamson6592@gmail.com>
2012-11-30 20:34:00 -0500

    should work now

--- gen/com/canefaitrien/gitpong/R.java
+++ gen/com/canefaitrien/gitpong/R.java
@@ -21,46 +21,15 @@ public final class R {
         public static final int m5dp=0x7f050004;
     }
     public static final class drawable {
-<<<<<<< HEAD
-        public static final int background_mainmenu=0x7f020000;
-        public static final int btn_buy_pressed=0x7f020001;
-        public static final int btn_buy_selected=0x7f020002;
-        public static final int btn_buy_unselected=0x7f020003;
-        public static final int btn_sell_pressed=0x7f020004;
-        public static final int btn_sell_selected=0x7f020005;
-        public static final int btn_sell_unselected=0x7f020006;
-        public static final int button_newgame_neutral=0x7f020007;
-        public static final int button_newgame_pressed=0x7f020008;
-        public static final int custom_btn_buy=0x7f020009;
-        public static final int custom_btn_main=0x7f02000a;
-        public static final int custom_btn_sell=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int menubutton_nuetral=0x7f02000d;
-        public static final int menubutton_pressed=0x7f02000e;
-        public static final int planet_a=0x7f02000f;
-        public static final int planet_a_small=0x7f020010;
-        public static final int planet_b=0x7f020011;
-        public static final int planet_b_small=0x7f020012;
-        public static final int planet_c=0x7f020013;
-        public static final int planet_c_small=0x7f020014;
-        public static final int planet_d=0x7f020015;
-        public static final int planet_e=0x7f020016;
-        public static final int planet_f=0x7f020017;
-        public static final int planet_g=0x7f020018;
-        public static final int planet_h=0x7f020019;
-        public static final int planet_i=0x7f02001a;
-        public static final int planet_j=0x7f02001b;
-        public static final int ship_icon=0x7f02001c;
-        public static final int ship_model_a=0x7f02001d;
-        public static final int ship_model_a_small=0x7f02001e;
-        public static final int space_trader_logo=0x7f02001f;
-        public static final int starfield_a=0x7f020020;
-        public static final int starfield_b=0x7f020021;
-=======
-        public static final int ball_a=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int paddle=0x7f020002;
->>>>>>> b63ff2fbf603ffa2c66840c6820c99227232564e
+        public static final int background_a=0x7f020000;
+        public static final int ball_a=0x7f020001;
+        public static final int custom_btn_main=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int menubutton_nuetral=0x7f020004;
+        public static final int menubutton_pressed=0x7f020005;
+        public static final int paddle=0x7f020006;
+        public static final int projectpong_logo=0x7f020007;
+        public static final int space_trader_logo=0x7f020008;
     }
     public static final class id {
         public static final int btn_debugmode=0x7f090003;
