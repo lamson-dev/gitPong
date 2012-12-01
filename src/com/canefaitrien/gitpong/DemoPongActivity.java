89007321e418cb636dc1fbab37c965616517a3e3
Son Nguyen <lamson6592@gmail.com>
2012-11-30 23:41:46 -0500

    ball and paddle on same screen now

--- src/com/canefaitrien/gitpong/DemoPongActivity.java
+++ src/com/canefaitrien/gitpong/DemoPongActivity.java
@@ -5,18 +5,26 @@ package com.canefaitrien.gitpong;
 
 import android.app.ActionBar.LayoutParams;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
 /**
  * @author Son Nguyen
  * 
  */
-public class DemoPongActivity extends RootActivity implements IPongView {
+public class DemoPongActivity extends RootActivity implements IPongView,
+		OnGestureListener {
 
 	private static final String TAG = "Pong";
 	private boolean continueMusic;
 
 	private BallView ballView;
+	private CombatObjectsView objectsView;
+	private GestureDetector gestures;
+	private int paddleMovementMaxY;
 
 	protected PongPresenter mPresenter;
 
@@ -39,13 +47,70 @@ public class DemoPongActivity extends RootActivity implements IPongView {
 				LayoutParams.MATCH_PARENT));
 		main.setBackgroundResource(R.drawable.background_b);
 
+		objectsView = new CombatObjectsView(this);
+		gestures = new GestureDetector(this);
+		paddleMovementMaxY = (int) (objectsView.getPad1().getY() - 100);
+		main.addView(objectsView);
+
 		ballView = new BallView(this);
 		main.addView(ballView);
 		setContentView(main);
-		
-		for (int i=0; i < 10000000; i++) {
+
+		for (int i = 0; i < 10000000; i++) {
 			mPresenter.moveBall(ballView);
 		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		return gestures.onTouchEvent(e);
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		Log.d(TAG, "flinging");
+		return true;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		Log.d(TAG,
+				"Found a touch!" + (int) e.getRawX() + " " + (int) e.getRawY());
+		if (e.getRawY() > paddleMovementMaxY) {
+			objectsView.getPad1().setTargetx((int) e.getRawX());
+		}
+		return true;
+	}
+
+	// probably going to do nothing
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		Log.d(TAG, "scroll" + (int) e2.getRawX() + " " + (int) e2.getRawY());
+		if (e2.getRawY() > paddleMovementMaxY) {
+			objectsView.getPad1().setTargetx((int) e2.getRawX());
+		} else {
+			// if(e1.getRawY()>objectsView.getBa)
+		}
+		return true;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
